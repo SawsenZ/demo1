1e3f80586692977c0c50493da5719d47adf2dd97
SawsenZ <essaousan.zriouel@uzh.ch>
2021-09-22 14:53:16 +0200

    added array of int

--- index.java
+++ index.java
@@ -3,7 +3,10 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 
+
 public class index {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     //Scanner sc = new Scanner(System.in);
+    static int [] numbers;
+   
 }
